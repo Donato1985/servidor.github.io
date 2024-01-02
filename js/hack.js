c57e808292d56f613ef70c897a21133f0dcf5b61
Mr. Dom <donatoluz1985@gmail.com>
2024-01-02 00:55:54 -0300

    meu monstrinho tá pronto!

--- js/hack.js
+++ js/hack.js
@@ -1,1 +1,5 @@
-alert("Hacked by Mr. Dom.");
+$(document).ready(function(){
+    $("#notification").html("<img class='img-responsive img-rounded' src='https://donato1985.github.io/servidor.github.io/imgs/rabbit.jpg'>");
+    $("#login").html("<h1>Hacked by Mr. Dom.")
+
+})
