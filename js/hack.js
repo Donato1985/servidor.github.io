6bbce22e5eb0d95a7704990d708462c8d7814c95
Mr. Dom <donatoluz1985@gmail.com>
2024-01-02 01:07:37 -0300

    melhorado

--- js/hack.js
+++ js/hack.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    $("#notification").html("<img class='img-responsive img-rounded' src='https://donato1985.github.io/servidor.github.io/imgs/rabbit.jpg'>");
-    $("#login").append("<h1>Hacked by Mr. Dom.</h1>")
+    $("#notification").html("<img class='img-responsive img-rounded' src='https://donato1985.github.io/servidor.github.io/imgs/rabbit.jpg'><br><h1>Hacked by Mr. Dom.</h1>");
 
 })
